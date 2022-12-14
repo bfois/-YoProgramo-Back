416e05f0122354d30abc76f7fe2c0b8bfef04f19
BraianF <97342723+bfois@users.noreply.github.com>
2022-12-14 08:13:01 -0300

    cors
    test

--- src/main/java/com/bef/api/Controller/AuthController.java
+++ src/main/java/com/bef/api/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://portfoliobf-d3405.web.app/","http://localhost:4200"})
+@CrossOrigin(origins = "https://portfoliobf-d3405.web.app/")
 @RequestMapping("/api/auth")
 public class AuthController {
 
